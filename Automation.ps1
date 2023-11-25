3c2a540f58ee73e4ee2c90a922779bad0ac1c06d
ChrisB2k00 <144250013+ChrisB2k00@users.noreply.github.com>
2023-11-25 00:00:23 +0000

    Update Automation.ps1

--- Automation.ps1
+++ Automation.ps1
@@ -3,37 +3,37 @@ $WindowsOSVersion = "Windows 11 22H2 x64"
 
 function Start-Autopilot {
 
-    Write-Host "Would you like to enrol this device into Autopilot?"
+    Write-Host "Would you like to enrol this device into Autopilot?" -ForegroundColor Magenta
 
     $APChoice = Read-Host "Y for Yes or N for No"
 
     if ($APChoice -eq "Y")
     {
-        Write-Host "Starting Autopilot Process"
+        Write-Host "Starting Autopilot Process" -ForegroundColor Yellow
         Start-AutopilotEnrolment
         Start-BuildSelection
     }
     elseif ($APChoice -eq "N")
     {
-        Write-Host "Skipping Autopilot Enrolment"
+        Write-Host "Skipping Autopilot Enrolment" -ForegroundColor Yellow
         Start-BuildSelection
     }
     else
     {
-        Write-Host "Invalid selection. Please try again."
+        Write-Host "Invalid selection. Please try again." -ForegroundColor Red
         Start-Autopilot
     }
 
 function Start-AutopilotEnrolment {
     $SerialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
 
-    Write-Host "Welcome to Autopilot Enrolment"
+    Write-Host "Welcome to Autopilot Enrolment" -ForegroundColor Yellow
     $GroupTag = Read-Host "Please enter your GroupTag (Case Sensitive)"
     $OutputFile = "X:\Autopilot\$SerialNumber.CSV"
     X:\Autopilot\Create_4kHash_using_OA3_Tool.ps1 -GroupTag $GroupTag -OutputFile $OutputFile
     ## NOT FINISHED - NEED TO EITHER GET IT TO ASK THE USER WHERE TO SAVE THE OUTPUT FILE, OR AUTOMATE IT UPLOADING
-    Write-Host "Creation of Autopilot CSV file succeeded!"
-    Write-Host "Starting Upload to Intune now via MS Graph. Please login with an account that has the ability to enrol devices to Autopilot."
+    Write-Host "Creation of Autopilot CSV file succeeded!" -ForegroundColor Green
+    Write-Host "Starting Upload to Intune now via MS Graph. Please login with an account that has the ability to enrol devices to Autopilot." -ForegroundColor Yellow
     Start-AutopilotGraphUpload
     }
 
@@ -142,39 +142,39 @@ function Start-AutopilotGraphUpload {
 }
     
 function Start-BuildSelection {
-    Write-Host "Which build would you like to run?"
-    Write-Host "1) Internal Build (Automated, will wipe the disk, install $WindowsOSVersion Enterprise, latest drivers and then shutdown"
-    Write-Host "2) Install specific WIM file (Prompt for WIM choice from available WIM files, wipe the disk, install Windows, latest drivers, and then shutdown)"
-    Write-Host "3) Show OSDCloud GUI for manual build"
+    Write-Host "Which build would you like to run?" -ForegroundColor Magenta
+    Write-Host "1) Internal Build (Automated, will wipe the disk, install $WindowsOSVersion Enterprise, latest drivers and then shutdown" -ForegroundColor Magenta
+    Write-Host "2) Install specific WIM file (Prompt for WIM choice from available WIM files, wipe the disk, install Windows, latest drivers, and then shutdown)" -ForegroundColor Magenta
+    Write-Host "3) Show OSDCloud GUI for manual build" -ForegroundColor Magenta
 
     $Choice = Read-Host "Please select which build you would like to run"
 
     if ($Choice -eq "1")
         {
-            Write-Host "Starting internal build"
+            Write-Host "Starting internal build" -ForegroundColor Yellow
             Start-OSDInternal
         }
     elseif ($Choice -eq "2")
         {
-            Write-Host "Starting specific WIM file build"
+            Write-Host "Starting specific WIM file build" -ForegroundColor Yellow
             Start-OSDWIM
         }
     elseif ($Choice -eq "3")
         {
-            Write-Host "Starting OSDCloud GUI"
+            Write-Host "Starting OSDCloud GUI" -ForegroundColor Yellow 
             Start-OSDCloudGUI
-            Write-Host -ForegroundColor Green "Build complete!"
+            Write-Host "Build complete!" -ForegroundColor Green
 
             ## Fix TPM Attestation issue seen before during Autopilot
             Start-TPMAttestationFix
 
-            Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!"
+            Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!" -ForegroundColor Yellow 
             Start-Sleep -Seconds 3
             wpeutil shutdown
         }
     else
         {
-            Write-Host "Invalid selection. Please try again."
+            Write-Host "Invalid selection. Please try again." -ForegroundColor Red
             Start-BuildSelection
         }
 
@@ -184,12 +184,12 @@ function Start-OSDInternal {
 
     ## Install Windows Version listed at top of script, Enterprise, GB language, allow taking screenshots, ZTI so no prompts, skip adding Autopilot profile JSON
     Start-OSDCloud -OSName "$WindowsOSVersion" -OSEdition Enterprise -OSLanguage en-GB -Screenshot -ZTI -SkipAutopilot
-    Write-Host -ForegroundColor Green "Build complete!"
+    Write-Host "Build complete!" -ForegroundColor Green
 
     ## Fix TPM Attestation issue seen before during Autopilot
     Start-TPMAttestationFix
 
-    Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!"
+    Write-Host "Shutting down in 3 seconds!" -ForegroundColor Yellow
     Start-Sleep -Seconds 3
     wpeutil shutdown
 }
@@ -201,22 +201,23 @@ function Start-OSDWIM {
 
     ## Starts OSDCloud with the parameters to search for WIM files, Skip adding Autopilot profile JSON, Skip ODT, and Zero Touch Installation (No prompts throughout build process)
     Start-OSDCloud -FindImageFile -SkipAutopilot -SkipODT -ZTI
-    Write-Host -ForegroundColor Green "Build complete!"
+    Write-Host "Build complete!" -ForegroundColor Green
 
     ## Fix TPM Attestation issue seen before during Autopilot
     Start-TPMAttestationFix
 
-    Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!"
+    Write-Host "Shutting down in 3 seconds!" -ForegroundColor Yellow
     Start-Sleep -Seconds 3
     wpeutil shutdown
 }
 
 function Start-TPMAttestationFix {
 
-    Write-Host -ForegroundColor Cyan "Adding registry key for TPM attestation fix"
+    Write-Host "Adding registry key for TPM attestation fix" -ForegroundColor Yellow
     reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OOBE /v SetupDisplayedEula /t REG_DWORD /d 00000001 /f
-    Write-Host -ForegroundColor Cyan "Reg key added!"
+    Write-Host "Reg key added!" -ForegroundColor Green
 }
 
-Write-Host "Automated Building Process V1.0" -ForegroundColor Yellow
+Write-Host "Automated Building Process V2.0" -ForegroundColor Yellow
+Write-Host "Now includes Autopilot Enrolment!" -ForegroundColor Yellow
 Start-Autopilot
